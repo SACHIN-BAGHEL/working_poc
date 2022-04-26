be8fadea89c81635917c5fb2f6b086a6689ebef0
Sachin Baghel <sachin.baghel@newvisionsoftware.in>
2022-04-26 16:02:18 +0530

    Optimization

--- widget-config-strapi/ui/widgets/widgets-dir/et-widget-config/src/api/Api.js
+++ widget-config-strapi/ui/widgets/widgets-dir/et-widget-config/src/api/Api.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+const domain = 'http://localhost:1337'
 
 export const postLoginAdmin = async (data) => {
-    return axios.post(`http://localhost:1337/admin/login`, data);
+    return axios.post(`${domain}/admin/login`, data);
 }
 
 // GET Collection Type
 export const getCollectionTypes = async (token) => {
-    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjQ3Mjc5NTA1LCJleHAiOjE2NDk4NzE1MDV9.qDiphqD-LOC6iXPfuIIMaS951f2jZG-z-rqgoqRIA6I'
-    const data = await axios.get(`http://localhost:1337/content-manager/content-types`, {
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjUwOTY5MDA3LCJleHAiOjE2NTM1NjEwMDd9.Pa__st_HHww_Ggn0n4jew9850rjSTkQ23JR1e-hPvU0'
+    const data = await axios.get(`${domain}/content-manager/content-types`, {
         headers: {
             'Authorization': `Bearer ${token}`
         }
@@ -16,14 +17,14 @@ export const getCollectionTypes = async (token) => {
 }
 
 export const getContents = async (collectionType) => {
-    let url = 'http://localhost:1337/api/' + collectionType;
+    let url = `${domain}/api/` + collectionType + '?populate=*';
     const data = await axios.get(url);
     return data;
 }
 
 export const fetchContents = async (collectionType) => {
     console.log("fetchContents", collectionType)
-    // const url = `http://localhost:1337/api/${collectionType}`;
+    // const url = `${domain}/api/${collectionType}`;
     return await getContents(collectionType);
 }
 
@@ -33,7 +34,7 @@ export const filterContentsByName = async (collectionType, contentName) => {
         throw new Error('collectionType or contentName is missing');
     }
 
-    let url = `http://localhost:1337/api/${collectionType}?filters[name][$containsi]=${contentName}`
+    let url = `${domain}/api/${collectionType}?filters[Title][$containsi]=${contentName}`
 
     return await axios.get(url)
 }
