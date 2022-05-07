1ae974a7a4ff251466a85586113c96055935e8c8
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2022-05-07 09:44:30 +0530

    Velocity Template Engine Adding.

--- widget-config-strapi/ui/widgets/widgets-dir/et-widget/src/App.js
+++ widget-config-strapi/ui/widgets/widgets-dir/et-widget/src/App.js
@@ -1,26 +1,54 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { getContentById, getTemplateById } from './api/Api';
+var velocityjs = require("velocityjs");
 import './app.css';
 
-const domain = 'http://localhost:1337'
+const domain = process.env.REACT_APP_STRAPI_API_URL;
 
 function App({ name, nameTwo, templateId, contentId }) {
     const [templateData, setTemplateData] = useState(null);
     const [contentData, setContentData] = useState(null);
-
-    console.log(name, nameTwo, templateId, contentId);
+    const [htmlCode, setHtmlCode] = useState(null);
 
     useEffect(async () => {
         const getTemplate = await getTemplateById(templateId ? templateId : 48);
         const getContent = await getContentById('banner', 1);
         setTemplateData(getTemplate);
         setContentData(getContent)
-    }, [])
 
+        // TODO: VELOCITY START
+
+        // TODO: for regex htmlCode.replace('/') 
+        // var s = '<div id="myDiv"></div>';
+        // var temp = document.createElement('div');
+        // temp.innerHTML = htmlCode;
+        // var htmlObject = temp.firstChild;
+        // htmlObject.querySelectorAll('img').forEach(
+        //     (el) => {
+        //         console.log('IMG', el);
+        //         // if (el.src.startsWith('$')) {
+        //             let templink = el.src
+        //             el.src = `${templink}`
+        //         // }
+        //     }
+        // );
+        if (getContent && getTemplate) {
+            const veloResult = velocityjs.render(getTemplate.contentShape, getContent);
+            setHtmlCode(veloResult);
+        }
+
+        // TODO: VELOCITY END
+
+    }, [])
     return (
-        <>
-            <div>{templateData ? JSON.stringify(templateData) : 'Loading OR Check Network'}</div>
-            <div>{templateData ? JSON.stringify(contentData) : 'Loading OR Check Network'}</div>
+        <>  
+            {
+                htmlCode ?
+                    <div dangerouslySetInnerHTML={{
+                        __html: htmlCode
+                    }}>
+                    </div> : <h1>You Fail</h1>
+            }
         </>
     );
 }
--- widget-config-strapi/ui/widgets/widgets-dir/et-widget/src/api/Api.js
+++ widget-config-strapi/ui/widgets/widgets-dir/et-widget/src/api/Api.js
@@ -13,7 +13,8 @@ export const getTemplate = async (searchby = 'code', searchTerm) => {
  * @returns 
  */
 export const getTemplateById = async (templateId) => {
-    return await axios.get(`${process.env.REACT_APP_PUBLIC_API_URL}/template/${templateId}`);
+    const { data } = await axios.get(`${process.env.REACT_APP_PUBLIC_API_URL}/template/${templateId}`);
+    return data;
 }
 
 /**
@@ -47,7 +48,7 @@ const getDefaultOptions = (defaultBearer) => {
         if (defaultBearer === 'EntKcToken') {
             return {
                 headers: {
-                    Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjUxODMzOTYxLCJleHAiOjE2NTQ0MjU5NjF9.JOpa49ii3d6-bbO8SNmE3oFYpdDEXtLVbQzJCl4_784'}`
+                    Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjUxODQwOTIyLCJleHAiOjE2NTQ0MzI5MjJ9.gs9bDgLlLZe0nFZowHuvK4DmDRLMrPIOpoH_luIc04o'}`
                 },
             }
         } else {
